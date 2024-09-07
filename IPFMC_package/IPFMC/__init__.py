9ec02549d467aebb159db6b255e2c769d3d98d4f
BioLemon <139327587+BioLemon@users.noreply.github.com>
2024-09-07 20:19:37 +0800

    Delete IPFMC_package directory

--- IPFMC_package/IPFMC/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '1.0.3'
-__author__ = 'Haoyang Zhang'
-__description__ = 'A tool for interpretable multi-omics integrated clustering.'
-
-__all__ = ['direct', 'separate', 'analysis']
-
-from . import *
-
